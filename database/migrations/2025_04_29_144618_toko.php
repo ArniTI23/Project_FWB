16ceb9e10112b81179f8636a6c1c80248faac3a3
Arni <arniqp03@gmail.com>
2025-05-05 00:10:17 +0800

    Membuat tabel di migrations

---
+++ database/migrations/2025_04_29_144618_toko.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_role');
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->text('alamat')->nullable();
+            $table->string('no_telepon')->nullable();
+            $table->foreignId('id_role')->constrained('roles')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('kategori', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kategori');
+            $table->timestamps();
+        });
+
+        Schema::create('produk', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_kategori')->constrained('kategori')->onDelete('cascade');
+            $table->string('nama_produk');
+            $table->text('deskripsi')->nullable();
+            $table->integer('stok');
+            $table->decimal('harga', 12, 2);
+            $table->string('foto_produk')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade'); // pembeli
+            $table->dateTime('tanggal');
+            $table->enum('status_pembayaran', ['pending', 'selesai', 'dibatalkan']);
+            $table->enum('status_pengiriman', ['belum dikirim', 'dikirim', 'diterima']);
+            $table->decimal('total_harga', 12, 2);
+            $table->timestamps();
+        });
+
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_transaksi')->constrained('transaksi')->onDelete('cascade');
+            $table->foreignId('id_produk')->constrained('produk')->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->decimal('subtotal', 12, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_transaksi');
+        Schema::dropIfExists('transaksi');
+        Schema::dropIfExists('produk');
+        Schema::dropIfExists('kategori');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+    }
+};
