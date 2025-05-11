29ac39560a209be1768990e75da97708301ebbe0
Arni <arniqp03@gmail.com>
2025-05-12 00:31:32 +0800

    update migration dan tabel

---
+++ database/migrations/2025_05_11_024441_transaksi_migration.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->string('metode_pembayaran');
+            $table->enum('status_pembayaran', ['pending', 'selesai', 'dibatalkan']);
+            $table->decimal('total_harga', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
